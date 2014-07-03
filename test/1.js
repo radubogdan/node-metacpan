0bfe51a84d16ae396593ecd918afbf38e5bb1f22
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-07-04 00:09:07 +0300

    implement function that return function that will make req on new endopoints

--- test/1.js
+++ test/1.js
@@ -2,6 +2,24 @@
 
 var mc = require('../lib/metacpan.js');
 
-mc.author("MG", function(name, err) {
+// Examples
+
+mc.author("MGV", function(name, err) {
     console.log(name || err);
 });
+
+mc.distribution("App-Devbot", function(name, err) {
+    console.log(name || err);
+});
+
+mc.file("/MGV/Mafia-0.001/lib/Mafia.pm", function(name, err) {
+    console.log(name || err);
+});
+
+mc.module("Mafia", function(name, err) {
+    console.log(name || err);
+});
+
+mc.release("Mafia", function(name, err) {
+    console.log(name || err)
+})
